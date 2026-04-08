142e759388881b69cfcb12b6fc605080f78e26d8
Atstincer <atstincer87@gmail.com>
2026-04-08 15:37:47 +0200

    adding test to check navigation front page to pokemon page

--- e2e-tests/mainpage.spec.js
+++ e2e-tests/mainpage.spec.js
@@ -6,4 +6,9 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('navigation from front page to pokemont page is possible', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
