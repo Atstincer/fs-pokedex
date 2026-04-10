9602fd94da831cb0ff50598b38635efca8378856
Atstincer <atstincer87@gmail.com>
2026-04-10 18:43:18 +0200

    adding health check endpoint

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
-app.get('/version', (req, res) => {
+/*app.get('/version', (req, res) => {
   res.send('100') // change this string to ensure a new version deployed
+})*/
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 const start = async () => {
